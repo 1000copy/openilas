06eec307ef17d504f533228b78beba113934189c
1000copy <1000copy@29a4d0e0-8715-11dd-b2bb-736fb4d8a922>
2008-09-20 16:02:35 +0000

    sqlite 基本环境搭建完毕。

---
+++ trunk/src/initdb.lua
@@ -0,0 +1,14 @@
+
+package.path = '..\\lib\\?.lua;'..package.path
+require "dbhelper"
+
+setconn("..\\bin\\openilas.db")
+createtable("employee",true,
+	{
+	   name = ftVarchar(10),
+	   code = ftVarchar(10),
+	   comment=ftVarchar(10),
+	})
+inserttable("employee",{name="lcj",code="001",comment="good man"})
+
+inserttable("employee",{name="sq",code="002",comment="good woman"})
