21430eefa36cebb73f0504d7f7c57942be121b39
1000copy <1000copy@29a4d0e0-8715-11dd-b2bb-736fb4d8a922>
2008-09-21 05:07:48 +0000

    5个小时乐战，完成读者，图书，员工的建模和管理，lua真的方便。 比如可以单文件运行，调试。
    下一步：借书。

---
+++ test/clidebug.cmd
@@ -0,0 +1,5 @@
+setb 0 c:\documents%and%settings\owner\my%documents\обть\activelua-9d00317d13c0\test\test-sqlite3.lua
+setb 0 c:\documents%and%settings\owner\my%documents\обть\activelua-9d00317d13c0\test\test-sqlite3.lua
+setb 33 c:\documents%and%settings\owner\my%documents\обть\activelua-9d00317d13c0\test\test-sqlite3.lua
+setb 59 d:\lcjun\openilas\test\testiup.wlua
+rootpath d:\lcjun\openilas\test
