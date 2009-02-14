45639f92e39541a0e8836ae32d6bdf441291c45c
1000copy <1000copy@29a4d0e0-8715-11dd-b2bb-736fb4d8a922>
2009-02-14 15:11:46 +0000

    准备迁移到sqlsmart上面，并且预计openilas将会在2010年面世，因此命名为OpenIlas 2010 。

---
+++ trunk/OpenIlas2010/OpenIlas/OpenIlas/UserList.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace openilas
+{
+    partial class UserList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.query = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.edit = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.password = new System.Windows.Forms.Button();
+            this.permit = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(903, 34);
+            this.panel1.TabIndex = 4;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Controls.Add(this.query);
+            this.flowLayoutPanel1.Controls.Add(this.add);
+            this.flowLayoutPanel1.Controls.Add(this.edit);
+            this.flowLayoutPanel1.Controls.Add(this.delete);
+            this.flowLayoutPanel1.Controls.Add(this.password);
+            this.flowLayoutPanel1.Controls.Add(this.permit);
+            this.flowLayoutPanel1.Controls.Add(this.exit);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(903, 29);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // query
+            // 
+            this.query.Location = new System.Drawing.Point(331, 3);
+            this.query.Name = "query";
+            this.query.Size = new System.Drawing.Size(75, 23);
+            this.query.TabIndex = 3;
+            this.query.Text = "query";
+            this.query.UseVisualStyleBackColor = true;
+            this.query.Click += new System.EventHandler(this.query_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(412, 3);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 6;
+            this.add.Text = "add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // edit
+            // 
+            this.edit.Location = new System.Drawing.Point(493, 3);
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(75, 23);
+            this.edit.TabIndex = 7;
+            this.edit.Text = "edit";
+            this.edit.UseVisualStyleBackColor = true;
+            this.edit.Click += new System.EventHandler(this.edit_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(574, 3);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 8;
+            this.delete.Text = "delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(655, 3);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(75, 23);
+            this.password.TabIndex = 9;
+            this.password.Text = "password";
+            this.password.UseVisualStyleBackColor = true;
+            this.password.Click += new System.EventHandler(this.password_Click);
+            // 
+            // permit
+            // 
+            this.permit.Location = new System.Drawing.Point(736, 3);
+            this.permit.Name = "permit";
+            this.permit.Size = new System.Drawing.Size(75, 23);
+            this.permit.TabIndex = 11;
+            this.permit.Text = "permit";
+            this.permit.UseVisualStyleBackColor = true;
+            this.permit.Click += new System.EventHandler(this.permit_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(817, 3);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 10;
+            this.exit.Text = "exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // grid
+            // 
+            this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Location = new System.Drawing.Point(5, 40);
+            this.grid.Name = "grid";
+            this.grid.RowTemplate.Height = 23;
+            this.grid.Size = new System.Drawing.Size(886, 233);
+            this.grid.TabIndex = 3;
+            // 
+            // UserList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 273);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.grid);
+            this.Name = "UserList";
+            this.Text = "UserList";
+            this.Load += new System.EventHandler(this.ReadList_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button query;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button edit;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button password;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button permit;
+        private System.Windows.Forms.DataGridView grid;
+
+    }
+}
