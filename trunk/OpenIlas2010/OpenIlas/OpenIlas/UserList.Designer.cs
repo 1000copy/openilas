eb4295a2959f3715e2827c421d40a0e38eac3a97
1000copy <1000copy@29a4d0e0-8715-11dd-b2bb-736fb4d8a922>
2009-04-26 13:42:33 +0000

    kick off again

--- trunk/OpenIlas2010/OpenIlas/OpenIlas/UserList.Designer.cs
+++ trunk/OpenIlas2010/OpenIlas/OpenIlas/UserList.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace openilas
+﻿namespace OpenIlas
 {
     partial class ReaderList
     {
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(862, 273);
+            this.ClientSize = new System.Drawing.Size(720, 267);
             this.Name = "ReaderList";
             this.Load += new System.EventHandler(this.ReaderList_Load);
             this.ResumeLayout(false);
